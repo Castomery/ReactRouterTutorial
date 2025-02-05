5a34f98d2c971e6237291352a6e643b88dda0f31
Vlad-Polygalov <vladpoligalov3@gmail.com>
2025-02-05 14:12:51 +0200

    Add server util and Vans page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { About } from './components/About'
 import { Root } from './components/Root'
 import { Home } from './components/Home'
+import { Vans } from './components/Vans'
+import './utils/server.js';
 
 function App() {
 
@@ -11,6 +13,7 @@ function App() {
         <Route path='/' element={<Root />}>
           <Route path='/' element={<Home/>}/>
           <Route path='/about' element={<About />} />
+          <Route path='/vans' element={<Vans />} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/components/Vans.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react"
+import '../css/components/Vans.css'
+
+type Van = {
+    id: string;
+    name: string;
+    price: number;
+    description: string;
+    imageUrl: string;
+    type: string;
+};
+
+export function Vans() {
+
+    const [vans, setVans] = useState<Van[]>([]);
+
+    useEffect(() => {
+        fetch("api/vans")
+            .then((res) => res.json())
+            .then((data) => setVans(data.vans))
+            .catch((err) => console.log("Error fetching vans:", err));
+    }, []);
+
+    const vanElements = vans.map(van => (
+        <section key={van.id} className="van-tile">
+            <img src={van.imageUrl} />
+            <section className="van-info">
+                <h3>{van.name}</h3>
+                <p>{van.price}<span>/day</span></p>
+            </section>
+            <i className={`van-type ${van.type} selected`}>{van.type}</i>
+        </section>
+    ));
+
+    return (
+    <div className="van-list-container">
+        <h1>Explor our van options</h1>
+        <div className="van-list">
+            {vanElements};
+        </div>
+    </div>);
+}
